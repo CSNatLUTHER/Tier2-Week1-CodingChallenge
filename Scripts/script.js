26b713edb9c2a61646784bd6598d261ebfaab46c
Chris Nelson <cnelson04@gmail.com>
2021-09-09 10:04:59 -0500

    challenge complete

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,5 +1,33 @@
 $(document).ready( onReady );
 
 function onReady(){
-    console.log("I'm Ready");
+    $( "#generatorButton" ).on ('click', createSection )
+    $( "#sectionDiv" ).on ( 'click', ".yellowButton", yellowFunciton )
+    $( "#sectionDiv" ).on ( 'click', ".deleteButton", deleteFunciton )
+}
+let counterID = 1;
+
+function createSection (){
+    $( "#sectionDiv" ).append(
+       `<div class="subSection">
+            <strong><p>${ counterID }</p></strong>
+            <input class="yellowButton" type="button" value="Yellow">
+            <input class="deleteButton" type="button" value="Delete">
+       </div>` 
+    )
+    counterID++
+}
+
+function yellowFunciton(){
+    $( this ).parent().toggleClass( 'yellow' );
+    if ($( this ).val() === "Yellow" ){
+        $( this ).val("Red"); 
+    }
+    else if ($( this ).val() === "Red" ){
+        $( this ).val("Yellow"); 
+    }
+}
+
+function deleteFunciton(){
+    $( this ).parent().slideUp(1000);
 }
